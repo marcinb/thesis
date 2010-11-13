34eddd1784ffa5f03a96038edadd3948d7e56b14
Marcin Balinski <balinski@ragnarson.com>
2010-11-13 13:16:59 +0100

    Modified generators to use haml & rpsec

--- blog/config/application.rb
+++ blog/config/application.rb
@@ -38,5 +38,11 @@ module Blog
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :haml
+      g.test_framework  :rspec, :fixture => false
+    end
   end
 end
