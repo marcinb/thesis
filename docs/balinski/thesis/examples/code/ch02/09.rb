a806377d91098cf12e82d6007d7b93c25e0edf19
Marcin Balinski <balinski@ragnarson.com>
2011-04-17 19:32:37 +0200

    Corrected listings formatting

--- docs/balinski/thesis/examples/code/ch02/09.rb
+++ docs/balinski/thesis/examples/code/ch02/09.rb
@@ -1,3 +1,5 @@
-[].should be_empty                      # prawda, [] jest pusta tablica
+[].should be_empty  # prawda, [] jest pusta tablica
+
 ["kot", "pies"].should include("malpa") # falsz, testowana tablica nie zawiera elementu "malpa"
-[].should be_instance_of(Array)         # prawda
+
+[].should be_instance_of(Array) # prawda
